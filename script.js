63fb9baf50b2110f0d195f9c262b11b43c104761
Rohit Pancholi <rohitpancholi14510@gmail.com>
2025-01-27 13:17:07 +0530

    chngs

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
 });
+// Scroll event for sticky header
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 150) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+  
+// Splide Slider
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#splide', {
+        type: 'loop',
+        perPage: 1,
+        autoplay: true,
+    }).mount();
+});
+  
